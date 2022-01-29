b85317c30b2eb0a2a6b34fca3aba4242d937ee06
Geetika Singh <78660964+Geetikaasingh@users.noreply.github.com>
2022-01-29 22:58:51 +0530

    Update HeroSection.js

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -5,7 +5,7 @@
  function HeroSection(){
      return(
          <div className='hero-container'>
-             <video src="/videos/Y2Mate.is - No Copyright Study Table with moving Elements Free Animated Background Back to School Education-0_CqqoB4CAs-1080p-1639962592938_Trim.mp4" autoPlay loop muted/>
+            <video src="/videos/video1.mp4" autoPlay loop muted/>
              <h1>Buy Rent Sell Books</h1>
              <h2>starting from ₹10 </h2>
              <h2>with FREE Home Delivery</h2>
